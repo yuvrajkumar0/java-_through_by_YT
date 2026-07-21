96cc82c1e6304de15f982f880a43e8031c83168b
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-21 18:22:20 +0530

    challenge_1

---
+++ challenge_1.java
@@ -0,0 +1,23 @@
+// write a java program to print the following pattern without using \n.
+ 
+
+public class challenge_1{
+    public static void main(String[] args){
+        System.out.println("*");
+          System.out.println("* *");
+            System.out.println("* * *");
+              System.out.println("* * * *");
+                System.out.println("* * * * *");
+
+
+         
+         System.out.println("\n* * * *");
+          System.out.println("* * *");
+           System.out.println("* * ");
+            System.out.println("*");
+ 
+         
+    
+    }
+}
+ 
