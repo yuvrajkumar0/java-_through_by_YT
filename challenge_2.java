1de3c3d5ec9bed9ca8a2f69322e64239a9b0d912
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-21 18:40:55 +0530

    challenge 2

---
+++ challenge_2.java
@@ -0,0 +1,26 @@
+// write a java program to use all arthmatic opertor.
+import java.util.Scanner;
+public class challenge_2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your first number:");
+        int a = input.nextInt();
+         System.out.print("enter your first second:");
+         int b = input.nextInt();
+         int sum = a+b;
+         int sub = a-b;
+         int mul = a*b;
+         int div = a/b;
+         System.out.println("your addition is:" + sum);
+         System.out.println("your subtract  is:" + sub);
+         System.out.println("your multiply is:" + mul);
+         System.out.println("your division is:" + div);
+          
+
+
+
+    }
+}
+
+ 
+
