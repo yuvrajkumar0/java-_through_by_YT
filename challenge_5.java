2a7f54deae12edb74eef3518080ca5478c7b52b8
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-22 22:34:54 +0530

    my challenge 5

---
+++ challenge_5.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+public class challenge_5{
+    public static void main(String[] args){
+        int arr[] = {11,12,13,14,15,16,17,19,20};
+        Scanner input = new Scanner(System.in);
+        System.out.print("please enter your find number: ");
+        int num = input.nextInt();
+         boolean isFound = isFound(arr, num);
+        if(isFound){
+             System.out.print("your number is found");
+        }else{
+             System.out.print("your number is not found");
+        }
+
+        
+        
+    }
+    public static boolean isFound(int arr[], int num){
+     int index = 0;
+     while(index< arr.length){
+        if(arr[index] == num){
+        return true;
+        }
+         index++;
+     }
+     return false;
+    }
+     
+}
