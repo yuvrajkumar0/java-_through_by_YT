aa65a7bd7c97d75c008ca8a22ba37deda66cfe53
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-23 23:23:38 +0530

    challenge 8

---
+++ challenge_8.java
@@ -0,0 +1,15 @@
+ public class challenge_8{
+    public static void main(String args[]){
+        int [][] myArr = {{1,2,3},{4,5,6}};
+       
+                
+                
+               for(int i=0; i<myArr.length; i++){
+                for(int j=0; j<myArr[i].length; j++){
+                    System.out.print(myArr[i][j] + "");
+                }
+                 System.out.print("\n");
+               }
+                  
+    }
+ }
