2f66c8f6ac86ede82785f77fb28655d16e43592a
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-08-02 23:50:38 +0530

    how to decleare variable

---
+++ variable.java
@@ -0,0 +1,18 @@
+public class variable{
+    public static void main(String[] args){
+        int myNumber;
+        myNumber=799;
+        System.out.println(myNumber);
+        myNumber=120;
+        System.out.println(myNumber);
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+        float raj = 4;
+        System.out.println(raj);
+        double myDouble = 4.222568;
+        System.out.println(myDouble);
+        String wishes = "good morning";
+        System.out.println(wishes);
+
+    }
+}
