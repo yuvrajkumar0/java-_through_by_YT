42a96755f7a4fd3e0e00eac89457b6cb6ec1a3e4
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-08-02 23:56:47 +0530

    learn loop

---
+++ loop.java
@@ -0,0 +1,17 @@
+ public class loop{
+    public static void main(String[] args){
+        int num = 1; 
+        while(num<=100){  
+            System.out.println(num);  
+            num = num+1;  
+              int i = 500;
+        }
+        
+    }
+}
+
+
+   
+       
+
+
