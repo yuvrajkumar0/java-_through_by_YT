324aa78823d8faa7237ac48baa9eb80f8fe80eb8
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-08-03 11:10:23 +0530

    add recursion method

--- Recursion.java
+++ Recursion.java
@@ -4,12 +4,21 @@ public class Recursion{
         Scanner input= new Scanner(System.in);
         System.out.print("Please enter your Number: ");
         int num = input.nextInt();
-        long fact = factroailNumber(num);
-        System.out.print(fact);
+        long fact1 = factroail(num);
+        long fact2= factroailNumber(num);
+        System.out.println(fact1);
+         System.out.print(fact2);
+    }
+    public static long factroail(int num){
+        System.out.println("Function call for " + num);
+        if(num==1){
+            return 1;
+        }
+        return num * factroail(num-1);
     }
     public static long factroailNumber(int num){
         long result = 1;
-        for(int i=1; i<num; i++){
+        for(int i=1; i<=num; i++){
          result = result * i;
         }
         return result;
