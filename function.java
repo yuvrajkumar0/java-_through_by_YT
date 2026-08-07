3ba01758f047b379f1ea2fe31e56437f30befb78
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-08-07 23:14:19 +0530

    use functions

---
+++ function.java
@@ -0,0 +1,31 @@
+ 
+import java.util.Scanner;
+public class function{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+         
+         System.out.print("please enter your number:");
+         int num = input.nextInt();
+         int reverse = reverse(num);
+         System.out.print(reverse);
+    }
+    public static int reverse(int num){
+        int newNum=0;
+        while(num>0){
+        int digit = num % 10;
+        newNum = newNum * 10 + digit;
+        num/=10;
+        
+        }
+        return newNum;
+       
+    }
+}
+
+
+
+ 
+     
+
+
+   
