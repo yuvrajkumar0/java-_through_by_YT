edc19679acd5092d46b0e67d794b01c196c21014
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-08-11 23:26:04 +0530

    abs,ceil,floor,round use

---
+++ math.java
@@ -0,0 +1,19 @@
+public class math{
+    public static void main(String args[]){
+        System.out.println("absulate = " + Math.abs(99));
+         System.out.println( Math.abs(99));
+          System.out.println( Math.ceil(5.047));
+           System.out.println(Math.floor(5.07));
+            System.out.print(Math.round(5.07));
+            System.out.println(Math.round(5.07));
+             
+
+             for(int i=0; i<10; i++){
+                int random =(int) (Math.random() * 100);
+                System.out.println(random);
+
+             }
+
+            
+    }
+}
